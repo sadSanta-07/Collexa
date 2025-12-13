ea04f896d78eb295c1966474057fd769e886a5e5
TesRekt-05 <iffcogc34@gmail.com>
2025-12-14 02:35:15 +0530

    feat: initialize Collexa project with Express server and basic API setup
    - Add package.json with dependencies and scripts for starting the server
    - Create server.js to configure and run the Express application
    - Implement app.js to set up middleware and a basic route for the API

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Collexa API is running!' });
+});
+
+module.exports = app;
